34fc1a1632dbd7a3ea37872cc7af94a0ccfb0c74
Raja Moukhass <moraja858@gmail.com>
2021-02-27 10:01:23 +0100

    modify tools_2.c

--- srcs/tools_2.c
+++ srcs/tools_2.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramoukha <moukhassraja@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/27 09:57:34 by ramoukha          #+#    #+#             */
+/*   Updated: 2021/02/27 10:00:02 by ramoukha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "rtv1.h"
 
-double ft_atof(char *str)
+double	ft_atof(char *str)
 {
 	int i;
 	int j;
 	int signe;
 	double res;
+
 	i = 0;
 	res = 0;
 	signe = 1;
 	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\t'
-	 || str[i] == '\v' || str[i] == '\r' || str[i] == '\n')
-	   i++;
+	|| str[i] == '\v' || str[i] == '\r' || str[i] == '\n')
+	i++;
 	if (str[i] == '+' || str[i] == '-')
 		signe = str[i++] == '-' ? -1 : 1;
 	while (str[i] <= '9' && str[i] >= '0')
