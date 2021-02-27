eec3406a5c35eca872f682254358a0fb22b4fac7
Raja Moukhass <moraja858@gmail.com>
2021-02-27 09:38:59 +0100

    add transform file

--- srcs/transforms.c
+++ srcs/transforms.c
@@ -1,39 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mv1in.c                                             :+:      :+:    :+:   */
+/*   transforms.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rv1moukhv1 <rv1moukhv1@student.42.fr>          +#+  +:+       +#+    */
+/*   By: ramoukha <moukhassraja@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 12:54:06 by ramoukha          #+#    #+#             */
-/*   Updated: 2021/02/18 15:50:26 by amya             ###   ########.fr       */
+/*   Updated: 2021/02/27 09:38:06 by ramoukha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "rtv1.h"
 
-t_vec ft_rotate_x(t_vec v, double angle){
+t_vec	ft_rotate_x(t_vec v, double angle)
+{
 	t_vec res;
 
 	res.x = v.x;
 	res.y = v.y * cos(angle) + v.z * sin(angle);
-	res.z = - v.y * sin(angle) + v.z * cos(angle);
+	res.z = -v.y * sin(angle) + v.z * cos(angle);
 	return (res);
 }
 
-t_vec ft_rotate_y(t_vec v, double angle)
+t_vec	ft_rotate_y(t_vec v, double angle)
 {
-	t_vec res;
+	t_vec	res;
+
 	res.y = v.y;
 	res.x = v.x * cos(angle) + v.z * sin(angle);
-	res.z = - v.x * sin(angle) + v.z * cos(angle);
+	res.z = -v.x * sin(angle) + v.z * cos(angle);
 	return (res);
-
 }
 
-t_vec   ft_transform_ray(t_obj *o)
+t_vec	ft_transform_ray(t_obj *o)
 {
-	t_vec rot;
-	t_vec trans;
+	t_vec	rot;
+	t_vec	trans;
 
 	o->axis = ft_rotate(o->axis, o->rot);
 	// o->axis.y = ft_translate(o->axis.y, trans);
@@ -41,9 +41,9 @@ t_vec   ft_transform_ray(t_obj *o)
 	return (o->axis);
 }
 
-t_vec ft_rotate_z(t_vec v, double angle)
+t_vec	ft_rotate_z(t_vec v, double angle)
 {
-	t_vec res;
+	t_vec	res;
 
 	res.z = v.z;
 	res.x = v.x * cos(angle) - v.y * sin(angle);
@@ -51,9 +51,9 @@ t_vec ft_rotate_z(t_vec v, double angle)
 	return (res);
 }
 
-t_vec ft_rotate(t_vec v, t_vec rot)
+t_vec	ft_rotate(t_vec v, t_vec rot)
 {
-	t_vec res;
+	t_vec	res;
 
 	res = ft_rotate_x(v, rot.x * M_PI / 180);
 	res = ft_rotate_y(res, rot.y * M_PI / 180);
@@ -61,8 +61,9 @@ t_vec ft_rotate(t_vec v, t_vec rot)
 	return (res);
 }
 
-t_vec ft_translate(t_vec v, t_vec trans){
-	t_vec res;
+t_vec	ft_translate(t_vec v, t_vec trans)
+{
+	t_vec	res;
 
 	res.x = v.x + trans.x;
 	res.y = v.y + trans.y;
