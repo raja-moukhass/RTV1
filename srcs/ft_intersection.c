5dc93d48c1bc0ce395605eaa1aafe574e9e8a193
raja moukhass <moraja858@gmail.com>
2021-03-09 18:11:45 +0100

    this belnogs to RT)

--- srcs/ft_intersection.c
+++ srcs/ft_intersection.c
@@ -6,7 +6,7 @@
 /*   By: ramoukha <ramoukha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 15:25:01 by ramoukha          #+#    #+#             */
-/*   Updated: 2021/03/02 17:54:27 by ramoukha         ###   ########.fr       */
+/*   Updated: 2021/03/09 18:09:00 by ramoukha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ double	cone_intersection(t_ray *ray, t_obj *cone)
 	t_sol	n;
 	t_vec	obj_center;
 	double	discr;
-
+	double t2 = -1;
+	double t3 = 1;
+	t_vec 	p1 = (t_vec){0,10,0};
+	t_vec p2 = (t_vec){0, -10,0};
 	n.k = tan((cone->an_ra * M_PI / 180.0));
 	obj_center = vec_sub(ray->o, cone->pos);
 	n.a = dot_product(ray->dir, ray->dir) - (1 + pow(n.k, 2)) *
@@ -30,13 +33,25 @@ double	cone_intersection(t_ray *ray, t_obj *cone)
 	* dot_product(obj_center, cone->axis)));
 	discr = n.b * n.b - 4 * n.a * n.c;
 	if (discr < 0)
-		return (-1);
-	else
+		return (0);
+	n.t1 = (-n.b - sqrt(discr)) / (2 * n.a);
+	n.t2 = (-n.b + sqrt(discr)) / (2 * n.a);
+	n.t1 =ft_min_ray(n.t1, n.t2, n.t);
+	if (n.t1 > 0)
 	{
-		n.t1 = (-n.b - sqrt(discr)) / (2 * n.a);
-		n.t2 = (-n.b + sqrt(discr)) / (2 * n.a);
+		t_vec q = vec_add(ray->o, vec_product(ray->dir, n.t1));
+		t2 = dot_product(cone->axis, vec_sub(q,p1));
+		t3 = dot_product(cone->axis, vec_sub(q,p2));
+
 	}
-	return (ft_min_ray(n.t1, n.t2, n.t));
+
+	if ( t2 < 0  && t3 > 0)
+	{
+		return (n.t1);
+		// return (t3);
+
+	}
+	return (0);
 }
 
 double	intersect_plane(t_ray *r, t_obj *plane)
@@ -61,6 +76,11 @@ double	intersection_cylinder(t_ray *ray, t_obj *cylinder)
 {
 	t_sol	n;
 	t_vec	x;
+	//double	m;
+	double t2 = -1;
+	double t3 = 1;
+	t_vec 	p1 = (t_vec){0,10,0};
+	t_vec p2 = (t_vec){0, -10,0};
 	double	delta;
 
 	x = vec_sub(ray->o, cylinder->pos);
@@ -77,7 +97,22 @@ double	intersection_cylinder(t_ray *ray, t_obj *cylinder)
 		return (0);
 	n.t1 = (-n.b - sqrt(delta)) / (2 * n.a);
 	n.t2 = (-n.b + sqrt(delta)) / (2 * n.a);
-	return (ft_min_ray(n.t1, n.t2, n.t));
+	n.t1 =ft_min_ray(n.t1, n.t2, n.t);
+	if (n.t1 > 0)
+	{
+		t_vec q = vec_add(ray->o, vec_product(ray->dir, n.t1));
+		t2 = dot_product(cylinder->axis, vec_sub(q,p1));
+		t3 = dot_product(cylinder->axis, vec_sub(q,p2));
+
+	}
+
+	if ( t2 < 0  && t3 > 0)
+	{
+		return (n.t1);
+		// return (t3);
+
+	}
+	return (0);
 }
 
 double	intersection_spher(t_ray *r, t_obj *s)
